f508807c5a0b53bd766cdb9db58d6995d343c450
You_80 <yugiri08@gmail.com>
2019-12-10 11:04:30 +0900

    発射音追加

--- Assets/Player chara Folder/PlayerController.cs
+++ Assets/Player chara Folder/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         this.rigid2D = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();
-        
+        //Componentを取得
     }
 
     // Update is called once per frame
@@ -27,14 +27,19 @@ public class PlayerController : MonoBehaviour
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
         //左右移動
+    
         int key = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
 
-
-        //プレイヤの速度
-        //スピード制限
-        if (animator.GetInteger("Status") != 3)
+            if (Input.GetKey(KeyCode.RightArrow)) { key = 1;
+            
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) { key = -1;
+            
+        }
+        
+            //プレイヤの速度
+            //スピード制限
+            if (animator.GetInteger("Status") != 3)
         {
             if (rigid2D.velocity.x > maxWalkSpeed && key == -1 || rigid2D.velocity.x < maxWalkSpeed * -1 && key == 1 || maxWalkSpeed*-1<rigid2D.velocity.x && rigid2D.velocity.x<maxWalkSpeed)
             {
@@ -61,5 +66,6 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetInteger("Status", 3);
         }
+        
     }
 }
